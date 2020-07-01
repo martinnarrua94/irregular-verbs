fe3da9657730a7decf41201e33709b384ac1fcb0
Martín Arrúa <martinnarrua94@gmail.com>
2020-07-01 07:39:04 -0300

    feat: material modules added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,18 @@ import { MatTableModule } from '@angular/material/table';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 const MaterialComponents = [
   MatMenuModule,
   MatTableModule,
   MatFormFieldModule,
-  MatInputModule];
+  MatInputModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonToggleModule];
 
 @NgModule({
   imports: [MaterialComponents],
