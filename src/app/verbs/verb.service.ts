7d0257d58f8095869adea4e73530389031174189
Martín Arrúa <martinnarrua94@gmail.com>
2020-07-09 17:56:01 -0300

    feat: getDefinition method added

--- src/app/verbs/verb.service.ts
+++ src/app/verbs/verb.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IVerbTenses } from '../models/verbTenses';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,21 @@ export class VerbService {
 
   private verbTensesUrl = 'assets/verbs.json';
 
+  private dictionaryUrl = 'https://api.dictionaryapi.dev/api/v1/entries/en/';
+
   constructor(private http: HttpClient) { }
 
   getVerbs(): Observable<IVerbTenses[]>{
     return this.http.get<IVerbTenses[]>(this.verbTensesUrl);
+  }
+
+  getDefinition(verb: string): Observable<any[]>{
+    return this.http.get<any[]>(this.dictionaryUrl+verb)
+    .pipe(
+      tap(response =>{
+        let result = response[0];
+        result.meaning.intransitiveVerb = result.meaning["intransitive verb"];
+        return result;
+      }));
   }
 }
