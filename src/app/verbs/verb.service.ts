53be41982b433734e2c14e99ced112e8c3389391
Martín Arrúa <martinnarrua94@gmail.com>
2020-07-14 20:39:58 -0300

    feat: getDefinition method completed

--- src/app/verbs/verb.service.ts
+++ src/app/verbs/verb.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IVerbTenses } from '../models/verbTenses';
-import { tap } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { IVerb } from '../models/verb';
+import { IMeaning } from '../models/meaning';
+import { IApiResponse } from '../models/apiResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -19,13 +22,36 @@ export class VerbService {
     return this.http.get<IVerbTenses[]>(this.verbTensesUrl);
   }
 
-  getDefinition(verb: string): Observable<any[]>{
-    return this.http.get<any[]>(this.dictionaryUrl+verb)
+  getDefinition(verb: string): Observable<IVerb[]>{
+    return this.http.get<IApiResponse[]>(this.dictionaryUrl+verb)
     .pipe(
-      tap(response =>{
-        let result = response[0];
-        result.meaning.intransitiveVerb = result.meaning["intransitive verb"];
+      map(response =>{
+        let meanings: IMeaning = response[0].meaning;
+        let result: IVerb[] = [];
+
+        if (meanings["verb"]){
+          meanings["verb"].forEach((verb: IVerb) => {
+            result.push(verb);
+          });
+        }
+        if (meanings["intransitive verb"]){
+          meanings["intransitive verb"].forEach((verb: IVerb) => {
+            result.push(verb);
+          });
+        }
+        if (meanings["transitive verb"]){
+          meanings["transitive verb"].forEach((verb: IVerb) => {
+            result.push(verb);
+          });
+        }
+        
         return result;
-      }));
+      }),
+      catchError(this.errorHandler)
+      );
   }
+
+  errorHandler(error: HttpErrorResponse) {
+    return Observable.throw(error.message || "server error.");
+}
 }
