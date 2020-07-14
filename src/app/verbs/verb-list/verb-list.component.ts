ae17e9db36f39ef8d8690ef6282e3453f546495c
Martín Arrúa <martinnarrua94@gmail.com>
2020-07-14 20:41:28 -0300

    feat: getDefinition method and expandedElement added

--- src/app/verbs/verb-list/verb-list.component.ts
+++ src/app/verbs/verb-list/verb-list.component.ts
@@ -2,11 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { IVerbTenses } from '../../models/verbTenses';
 import { VerbService } from '../verb.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { IVerb } from '../../models/verb';
 
 @Component({
   selector: 'app-verb-list',
   templateUrl: './verb-list.component.html',
-  styleUrls: ['./verb-list.component.css']
+  styleUrls: ['./verb-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 
 export class VerbListComponent implements OnInit {
@@ -17,6 +26,12 @@ export class VerbListComponent implements OnInit {
   
   dataSource = new MatTableDataSource<IVerbTenses>();
   originalDataSource = new MatTableDataSource<IVerbTenses>();
+
+  expandedElement: string | undefined;
+
+  verbDefinition: IVerb[] | undefined;
+
+  errorMessage = '';
   
   constructor(private verbService: VerbService) { }
 
@@ -32,6 +47,22 @@ export class VerbListComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  getDefinition(verb: string){
+    this.verbDefinition = undefined;
+    this.errorMessage = '';
+    this.verbService.getDefinition(verb).subscribe({
+      next: verbDefinition => {
+        debugger;
+        this.verbDefinition = verbDefinition;
+        console.log(this.verbDefinition);
+      },
+      error: err => {
+        this.errorMessage = err;
+        console.log(this.errorMessage)
+      }
+    }); 
   }
 
   startsWith(event: Event){
