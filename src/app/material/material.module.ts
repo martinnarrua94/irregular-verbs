8704056a66832a77d611d08a375c8c9f60c9d142
Martín Arrúa <martinnarrua94@gmail.com>
2020-07-14 20:38:18 -0300

    feat: material modules added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,7 +5,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const MaterialComponents = [
   MatMenuModule,
@@ -14,7 +16,9 @@ const MaterialComponents = [
   MatInputModule,
   MatToolbarModule,
   MatIconModule,
-  MatButtonToggleModule];
+  MatButtonToggleModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule];
 
 @NgModule({
   imports: [MaterialComponents],
