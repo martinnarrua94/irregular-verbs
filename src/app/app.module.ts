95cbac13f51d04a4f4a7288b8ddba27d228677c9
Martín Arrúa <martinnarrua94@gmail.com>
2020-07-18 11:30:29 -0300

    feat: cover component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { CoverComponent } from './shared/cover/cover.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { HeaderComponent } from './shared/header/header.component';
     AppComponent,
     VerbListComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/cover/cover.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input,  } from '@angular/core';
+
+@Component({
+  selector: 'app-cover',
+  templateUrl: './cover.component.html',
+  styleUrls: ['./cover.component.css']
+})
+export class CoverComponent {
+
+  constructor() { }
+
+  @Input() title: string = '';
+
+}
